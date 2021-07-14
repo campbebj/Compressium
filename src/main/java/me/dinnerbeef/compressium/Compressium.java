0ade5fe568fc03ae27d0c145cbf3509600d5dc77
campbebj <ben.campbell95@gmail.com>
2021-07-13 19:00:22 -0700

    Fixing Blocktype not being maintained with new generation method

--- src/main/java/me/dinnerbeef/compressium/Compressium.java
+++ src/main/java/me/dinnerbeef/compressium/Compressium.java
@@ -13,9 +13,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod("compressium")
+import java.util.HashMap;
+import java.util.Map;
+
+@Mod(Compressium.MODID)
 public class Compressium {
-    public static final CreativeModeTab creativeTab = new CreativeModeTab("compressium") {
+    public static final String MODID = "compressium";
+    public static final Map<String, Block[]> BLOCKS = new HashMap<>();
+
+    public static final CreativeModeTab creativeTab = new CreativeModeTab(Compressium.MODID) {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("compressium:cobblestone_1")));
@@ -36,9 +42,12 @@ public class Compressium {
 
     private void registerBlocks(RegistryEvent.Register<Block> event) {
         for (CompressiumType type : CompressiumType.VALUES) {
+            Block[] compressedList = new Block[9];
+            BLOCKS.put(type.name, compressedList);
             for (int i = 0; i < 9; i++) {
-                Block block = type.factory.get();
+                Block block = type.getBlock();
                 event.getRegistry().register(block.setRegistryName(type.name + "_" + (i + 1)));
+                compressedList[i] = block;
                 type.blocks.add(block);
             }
         }
--- src/main/java/me/dinnerbeef/compressium/CompressiumType.java
+++ src/main/java/me/dinnerbeef/compressium/CompressiumType.java
@@ -1,47 +1,164 @@
 package me.dinnerbeef.compressium;
 
-import me.dinnerbeef.compressium.blocks.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.NetherrackBlock;
+import net.minecraft.world.level.block.PoweredBlock;
+import net.minecraft.world.level.block.SoulSandBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 public enum CompressiumType {
-    COBBLESTONE("cobblestone", Cobblestone::new),
-    STONE("stone", Stone::new),
-    SAND("sand", Sand::new),
-    GRAVEL("gravel", Gravel::new),
-    NETHERRACK("netherrack", Netherrack::new),
-    SNOW("snow", Snow::new),
-    SOULSAND("soulsand", Soulsand::new),
-    IRON("iron", Iron::new),
-    GOLD("gold", Gold::new),
-    DIAMOND("diamond", Diamond::new),
-    EMERALD("emerald", Emerald::new),
-    CLAY("clay", Clay::new),
-    NETHERITE("netherite", Netherite::new),
-    DIRT("dirt", Dirt::new),
-    COAL("coal", Coal::new),
-    REDSAND("redsand", RedSand::new),
-    ENDSTONE("endstone", Endstone::new),
-    OBSIDIAN("obsidian", Obsidian::new),
-    LAPIS("lapis", Lapis::new),
-    QUARTZ("quartz", Quartz::new),
-    REDSTONE("redstone", Redstone::new),
-    ANDESITE("andesite", Andesite::new),
-    DIORITE("diorite", Diorite::new),
-    GRANITE("granite", Granite::new);
+    COBBLESTONE("cobblestone",
+            new ResourceLocation("minecraft", "cobblestone"),
+            new ResourceLocation("minecraft", "block/cobblestone"),
+            new ResourceLocation("minecraft","block/cobblestone"),
+            Block::new),
+    STONE("stone",
+            new ResourceLocation("minecraft", "stone"),
+            new ResourceLocation("minecraft", "block/stone"),
+            new ResourceLocation("minecraft","block/stone"),
+            Block::new),
+    SAND("sand",
+            new ResourceLocation("minecraft", "sand"),
+            new ResourceLocation("minecraft", "block/sand"),
+            new ResourceLocation("minecraft","block/sand"),
+            FallingDamageBlock::new),
+    GRAVEL("gravel",
+            new ResourceLocation("minecraft", "gravel"),
+            new ResourceLocation("minecraft", "block/gravel"),
+            new ResourceLocation("minecraft","block/gravel"),
+            FallingDamageBlock::new),
+    NETHERRACK("netherrack",
+            new ResourceLocation("minecraft", "netherrack"),
+            new ResourceLocation("minecraft", "block/netherrack"),
+            new ResourceLocation("minecraft","block/netherrack"),
+            NetherrackBlock::new),
+    SNOW("snow",
+            new ResourceLocation("minecraft", "snow"),
+            new ResourceLocation("minecraft", "block/snow"),
+            new ResourceLocation("minecraft","block/snow_block"),
+            Block::new),
+    SOULSAND("soulsand",
+            new ResourceLocation("minecraft", "soul_sand"),
+            new ResourceLocation("minecraft", "block/soul_sand"),
+            new ResourceLocation("minecraft","block/soul_sand"),
+            SoulSandBlock::new),
+    IRON("iron",
+            new ResourceLocation("minecraft", "iron_block"),
+            new ResourceLocation("minecraft", "block/iron_block"),
+            new ResourceLocation("minecraft","block/iron_block"),
+            Block::new),
+    GOLD("gold",
+            new ResourceLocation("minecraft", "gold_block"),
+            new ResourceLocation("minecraft", "block/gold_block"),
+            new ResourceLocation("minecraft","block/gold_block"),
+            Block::new),
+    DIAMOND("diamond",
+            new ResourceLocation("minecraft", "diamond_block"),
+            new ResourceLocation("minecraft", "block/diamond_block"),
+            new ResourceLocation("minecraft","block/diamond_block"),
+            Block::new),
+    EMERALD("emerald",
+            new ResourceLocation("minecraft", "emerald_block"),
+            new ResourceLocation("minecraft", "block/emerald_block"),
+            new ResourceLocation("minecraft","block/emerald_block"),
+            Block::new),
+    CLAY("clay",
+            new ResourceLocation("minecraft", "clay"),
+            new ResourceLocation("minecraft", "block/clay"),
+            new ResourceLocation("minecraft","block/clay"),
+            Block::new),
+    NETHERITE("netherite",
+            new ResourceLocation("minecraft", "netherite_block"),
+            new ResourceLocation("minecraft", "block/netherite_block"),
+            new ResourceLocation("minecraft","block/netherite_block"),
+            Block::new),
+    DIRT("dirt",
+            new ResourceLocation("minecraft", "dirt"),
+            new ResourceLocation("minecraft", "block/dirt"),
+            new ResourceLocation("minecraft","block/dirt"),
+            Block::new),
+    COAL("coal",
+            new ResourceLocation("minecraft", "coal_block"),
+            new ResourceLocation("minecraft", "block/coal_block"),
+            new ResourceLocation("minecraft","block/coal_block"),
+            Block::new),
+    REDSAND("redsand",
+            new ResourceLocation("minecraft", "red_sand"),
+            new ResourceLocation("minecraft", "block/red_sand"),
+            new ResourceLocation("minecraft","block/red_sand"),
+            FallingDamageBlock::new),
+    ENDSTONE("endstone",
+            new ResourceLocation("minecraft", "end_stone"),
+            new ResourceLocation("minecraft", "block/end_stone"),
+            new ResourceLocation("minecraft","block/end_stone"),
+            Block::new),
+    OBSIDIAN("obsidian",
+            new ResourceLocation("minecraft", "obsidian"),
+            new ResourceLocation("minecraft", "block/obsidian"),
+            new ResourceLocation("minecraft","block/obsidian"),
+            Block::new),
+    LAPIS("lapis",
+            new ResourceLocation("minecraft", "lapis_block"),
+            new ResourceLocation("minecraft", "block/lapis_block"),
+            new ResourceLocation("minecraft","block/lapis_block"),
+            Block::new),
+    QUARTZ("quartz",
+            new ResourceLocation("minecraft", "quartz_block"),
+            new ResourceLocation("minecraft", "block/quartz_block_side"),
+            new ResourceLocation("minecraft","block/quartz_block"),
+            Block::new),
+    REDSTONE("redstone",
+            new ResourceLocation("minecraft", "redstone_block"),
+            new ResourceLocation("minecraft", "block/redstone_block"),
+            new ResourceLocation("minecraft","block/redstone_block"),
+            PoweredBlock::new),
+    ANDESITE("andesite",
+            new ResourceLocation("minecraft", "andesite"),
+            new ResourceLocation("minecraft", "block/andesite"),
+            new ResourceLocation("minecraft","block/andesite"),
+            Block::new),
+    DIORITE("diorite",
+            new ResourceLocation("minecraft", "diorite"),
+            new ResourceLocation("minecraft", "block/diorite"),
+            new ResourceLocation("minecraft","block/diorite"),
+            Block::new),
+    GRANITE("granite",
+            new ResourceLocation("minecraft", "granite"),
+            new ResourceLocation("minecraft", "block/granite"),
+            new ResourceLocation("minecraft","block/granite"),
+            Block::new);
 
     public static final CompressiumType[] VALUES = values();
 
     public final String name;
-    public final Supplier<Block> factory;
+    public final ResourceLocation baseResourceLocation;
     public final List<Block> blocks;
+    public final ResourceLocation particlePath;
+    public final ResourceLocation baseBlockModel;
+    private final Function<BlockBehaviour.Properties, Block> constructor;
 
-    CompressiumType(String n, Supplier<Block> f) {
-        name = n;
-        factory = f;
-        blocks = new ArrayList<>();
+    CompressiumType(String n, ResourceLocation baseResourceLocation,
+                    ResourceLocation particlePath, ResourceLocation baseBlockModel,
+                    Function<BlockBehaviour.Properties, Block> constructor) {
+        this.name = n;
+        this.blocks = new ArrayList<>();
+        this.particlePath = particlePath;
+        this.baseBlockModel = baseBlockModel;
+        this.baseResourceLocation = baseResourceLocation;
+        this.constructor = constructor;
+    }
+
+    public Block getBaseBlock() {
+        return ForgeRegistries.BLOCKS.getValue(baseResourceLocation);
+    }
+
+    public Block getBlock() {
+        return constructor.apply(BlockBehaviour.Properties.copy(ForgeRegistries.BLOCKS.getValue(baseResourceLocation)));
     }
 }
