494fd65d3e455697d18bad410086c66c5087aee7
Dinnerbeef <DinnerBeef18@gmail.com>
2021-07-26 13:21:58 -0500

    Removed Unused Import

--- src/main/java/me/dinnerbeef/compressium/blocks/Clay.java
+++ src/main/java/me/dinnerbeef/compressium/blocks/Clay.java
@@ -1,6 +1,5 @@
 package me.dinnerbeef.compressium.blocks;
 
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.Material;
