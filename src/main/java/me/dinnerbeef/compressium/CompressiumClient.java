415a455d39d2a612ac9bee18cb530325a06dbcc0
Dinnerbeef <DinnerBeef18@gmail.com>
2021-07-26 13:20:13 -0500

    Cleaned Up Code

--- src/main/java/me/dinnerbeef/compressium/CompressiumClient.java
+++ src/main/java/me/dinnerbeef/compressium/CompressiumClient.java
@@ -9,8 +9,7 @@ public class CompressiumClient extends CompressiumCommon {
     public static void setupItemVar() {
         for (CompressiumType type : CompressiumType.VALUES) {
             for (Block block : type.blocks) {
-                ItemBlockRenderTypes.setRenderLayer(block,
-                                 renderType -> renderType == RenderType.solid() || renderType == RenderType.translucent());
+                ItemBlockRenderTypes.setRenderLayer(block, renderType -> renderType == RenderType.solid() || renderType == RenderType.translucent());
             }
         }
     }
