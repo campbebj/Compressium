6f6d0ec9b3616e64f8b0dbb0c39e66da1fb6f6a9
campbebj <ben.campbell95@gmail.com>
2021-07-31 17:08:18 -0700

    fixing line endings

--- src/main/java/me/dinnerbeef/compressium/generators/CompressiumItemModelProvider.java
+++ src/main/java/me/dinnerbeef/compressium/generators/CompressiumItemModelProvider.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package me.dinnerbeef.compressium.generators;
 
 import me.dinnerbeef.compressium.Compressium;
@@ -21,3 +22,28 @@ public class CompressiumItemModelProvider extends ItemModelProvider {
         }
     }
 }
+=======
+package me.dinnerbeef.compressium.generators;
+
+import me.dinnerbeef.compressium.Compressium;
+import me.dinnerbeef.compressium.CompressiumType;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class CompressiumItemModelProvider extends ItemModelProvider {
+    public CompressiumItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, Compressium.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        for (CompressiumType type : CompressiumType.VALUES) {
+            for (int i = 1; i <= 9; i++) {
+                withExistingParent(Compressium.MODID+":"+type.name+"_"+i, new ResourceLocation(Compressium.MODID, "block/" + type.name+"_"+i));
+            }
+        }
+    }
+}
+>>>>>>> Stashed changes
