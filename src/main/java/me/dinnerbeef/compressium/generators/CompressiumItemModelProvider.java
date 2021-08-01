e3691fb71bb6d8c38725554d3bbf5c77468a8418
campbebj <ben.campbell95@gmail.com>
2021-07-31 17:25:29 -0700

    fixing merge issue

--- src/main/java/me/dinnerbeef/compressium/generators/CompressiumItemModelProvider.java
+++ src/main/java/me/dinnerbeef/compressium/generators/CompressiumItemModelProvider.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package me.dinnerbeef.compressium.generators;
 
 import me.dinnerbeef.compressium.Compressium;
@@ -22,28 +21,4 @@ public class CompressiumItemModelProvider extends ItemModelProvider {
         }
     }
 }
-=======
-package me.dinnerbeef.compressium.generators;
 
-import me.dinnerbeef.compressium.Compressium;
-import me.dinnerbeef.compressium.CompressiumType;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
-import net.minecraftforge.common.data.ExistingFileHelper;
-
-public class CompressiumItemModelProvider extends ItemModelProvider {
-    public CompressiumItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
-        super(generator, Compressium.MODID, existingFileHelper);
-    }
-
-    @Override
-    protected void registerModels() {
-        for (CompressiumType type : CompressiumType.VALUES) {
-            for (int i = 1; i <= 9; i++) {
-                withExistingParent(Compressium.MODID+":"+type.name+"_"+i, new ResourceLocation(Compressium.MODID, "block/" + type.name+"_"+i));
-            }
-        }
-    }
-}
->>>>>>> Stashed changes
